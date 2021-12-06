026c4b804b1c749ca6c48766cb993c3f66572d45
quinnciples <49799428+quinnciples@users.noreply.github.com>
2021-12-05 20:58:41 -0700

    Updated sphere intersection code to handle intersections happening within a sphere object.

--- SpherePrimitive.py
+++ SpherePrimitive.py
@@ -9,6 +9,9 @@ class SpherePrimitive(Primitive):
     c = np.linalg.norm(ray_origin - center) ** 2 - radius ** 2
     c = ((ray.origin - self.center).length ** 2) - (self.radius ** 2)
     """
+
+    EPSILON = 1e-5  # 0.000000001
+
     def __init__(self, position: Q_Vector3d, ambient: Q_Vector3d, diffuse: Q_Vector3d, specular: Q_Vector3d, shininess: float, reflection: float, radius: float):
         # self.center = center
         Primitive.__init__(self, position=position, ambient=ambient, diffuse=diffuse, specular=specular, shininess=shininess, reflection=reflection)
@@ -24,13 +27,34 @@ class SpherePrimitive(Primitive):
         # radius = self.radius
         # c = np.linalg.norm(ray_origin - center) ** 2 - radius ** 2
 
-        delta = b ** 2 - 4 * c
-        if delta > 0:
-            t1 = (-b + math.sqrt(delta)) / 2
-            t2 = (-b - math.sqrt(delta)) / 2
-            if t1 > 0 and t2 > 0:
-                distance = min(t1, t2)
-                intersection_point = ray.origin + ray.direction * distance
-                normal_to_surface = (intersection_point - self.position).normalized()  # What if we're inside the sphere?
-                return distance, normal_to_surface
-        return None, None
+        # delta = b ** 2 - 4 * c
+        # if delta > 0:
+        #     t1 = (-b + math.sqrt(delta)) / 2
+        #     t2 = (-b - math.sqrt(delta)) / 2
+        #     print(b, c, delta, t1, t2)
+        #     if t1 > 0 and t2 > 0:
+        #         distance = min(t1, t2)
+        #         intersection_point = ray.origin + ray.direction * distance
+        #         normal_to_surface = (intersection_point - self.position).normalized()  # What if we're inside the sphere?
+        #         return distance, normal_to_surface
+
+        op = self.position - ray.origin
+        radiusSquared = self.radius ** 2
+        b = op.dot_product(ray.direction)
+        determinant = b * b - (op.length * op.length) + radiusSquared
+        if (determinant < 0):
+            return None, None
+
+        determinant = math.sqrt(determinant)
+        minusT = b - determinant
+        plusT = b + determinant
+        if (minusT < SpherePrimitive.EPSILON and plusT < SpherePrimitive.EPSILON):
+            return None, None
+
+        t = minusT if minusT > SpherePrimitive.EPSILON else plusT
+        hitPosition = ray.origin + (ray.direction * t)
+        normal = (hitPosition - self.position).normalized()
+        inside = normal.dot_product(ray.direction) > 0
+        if inside:
+            normal = normal * -1
+        return t, normal
