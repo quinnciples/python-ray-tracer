1c13dff5107166d0b9f808e87076b123198a6abf
quinnciples <49799428+quinnciples@users.noreply.github.com>
2021-12-16 17:45:12 -0700

    Added recursive trace_ray member function to Scene.

--- Scene.py
+++ Scene.py
@@ -21,18 +21,27 @@ class Scene:
     def nearest_intersection(self, ray: Ray) -> tuple[Primitive, float, Q_Vector3d]:  # Could this be optimized by asking "does a ray of length(this_distance so far) intersect this object" ?
         min_distance = math.inf
         obj = None
-        normal_to_surface = None
-        is_inside = None
+        nearest_hit = None
         for object in self.objects:
-            # this_distance, this_normal_to_surface = object.intersect(ray=ray)
             hit = object.intersect(ray=ray)
             if hit and hit.distance < min_distance:
-                min_distance = hit.distance
                 obj = object
-                normal_to_surface = hit.normal_to_surface
-                is_inside = hit.is_inside
-        return obj, Hit(position=ray.position_at_distance(min_distance), distance=min_distance, normal_to_surface=normal_to_surface, is_inside=is_inside)
+                nearest_hit = hit
+                min_distance = hit.distance
+        return obj, nearest_hit
 
+    @staticmethod
+    def write_ppm_file(image_data) -> None:
+        height = len(image_data)
+        width = len(image_data[0])
+        # PPM format
+        with open("image.ppm", "w") as pic:
+            pic.write(f"P3\n{width} {height}\n255\n")
+            for row in image_data:
+                for col in row:
+                    pic.write(f"{int(col[0] * 255)} {int(col[1] * 255)} {int(col[2] * 255)} ")
+                pic.write("\n")
+    
     def multi_render(self, width: int, height: int, max_depth: int = 1, anti_aliasing: bool = False, lighting_samples: int = 1, cores_to_use: int = 1) -> None:
         number_of_buckets = 10
         cores_to_use = max(cores_to_use, 1) if cores_to_use != 0 else cpu_count()
@@ -49,17 +58,11 @@ class Scene:
         for result in results:
             image += result
         plt.imsave('image.png', image)
-        # PPM format
-        with open("image.ppm", "w") as pic:
-            pic.write(f"P3\n{width} {height}\n255\n")
-            image_data = image.tolist()
-            for row in image_data:
-                for col in row:
-                    pic.write(f"{int(col[0] * 255)} {int(col[1] * 255)} {int(col[2] * 255)} ")
-                pic.write("\n")
+        Scene.write_ppm_file(image_data = image.tolist())
 
     def render(self, width: int, height: int, max_depth: int = 1, anti_aliasing: bool = False, lighting_samples: int = 1, row_range: dict = {}) -> np.array:
         image = np.zeros((height, width, 3))
+        self.lighting_samples = lighting_samples
         SCREEN_RATIO = float(width) / float(height)
         SCREEN_DIMS = {'left': -1, 'top': 1 / SCREEN_RATIO, 'right': 1, 'bottom': -1 / SCREEN_RATIO}
 
@@ -107,59 +110,62 @@ class Scene:
                     pixel = Q_Vector3d(xx + ANTI_ALIASING_OFFSETS[offset][0], yy + ANTI_ALIASING_OFFSETS[offset][1], 0)
                     origin = self.camera_position
                     direction = (pixel - origin).normalized()
-                    reflection = 1.0
-
-                    for _ in range(max_depth):
-
-                        ray = Ray(origin=origin, direction=direction)
-                        nearest_object, object_hit = self.nearest_intersection(ray=ray)
-
-                        # Did we even hit anything?
-                        if nearest_object is None:
-                            break
-
-                        # Did we hit a light?
-                        # if nearest_object.emission != Q_Vector3d(0, 0, 0):
-                        #     color_value = nearest_object.emission  # * reflection?
-                        #     break
-
-                        intersection_point = origin + direction * object_hit.distance
-                        shifted_point = intersection_point + object_hit.normal_to_surface * 1e-5
-                        direction_from_intersection_to_light = (self.lights[0]['position'] - shifted_point).normalized()
-
-                        # Lighting
-                        illumination = self.calculate_lighting(lighting_samples=lighting_samples, this_object=nearest_object, origin=shifted_point, direction_from_intersection_to_light=direction_from_intersection_to_light, object_hit=object_hit)
-
-                        # Reflection
-                        color_value += illumination * reflection
-
-                        # TODO - testing on whether this is necessary or not.
-                        # if not hit_light:
-                        #     break
-
-                        # Handle reflection and continue
-                        reflection *= nearest_object.reflection
-                        if reflection == 0:
-                            break
-
-                        # Reset origination and direction to this point and continue recursively
-                        origin = shifted_point
-                        direction = direction.reflected(other_vector=object_hit.normal_to_surface)
+                    color_value += self.trace_ray(ray=Ray(origin=origin, direction=direction), max_depth=max_depth)
 
                 image[y, x] = (color_value * (1 / (num_samples + 1))).clamp(0, 1).to_tuple()
 
         return image
 
-    def calculate_lighting(self, lighting_samples: int, this_object: Primitive, origin: Q_Vector3d, direction_from_intersection_to_light: Q_Vector3d, object_hit: Hit) -> Q_Vector3d:
+    def trace_ray(self, ray: Ray, max_depth: int, current_depth: int = 1, reflection: float = 1.0) -> Q_Vector3d:
+
+        nearest_object, object_hit = self.nearest_intersection(ray=ray)
+        color_value = Q_Vector3d(0, 0, 0)
+
+        # Did we even hit anything?
+        if nearest_object is None:
+            return color_value
+
+        # Did we hit a light?
+        # if nearest_object.emission != Q_Vector3d(0, 0, 0):
+        #     color_value = nearest_object.emission  # * reflection?
+        #     break
+
+        shifted_point = object_hit.position + object_hit.normal_to_surface * 1e-5
+        direction_from_intersection_to_light = (self.lights[0]['position'] - shifted_point).normalized()
+
+        # Lighting
+        illumination = self.calculate_lighting(this_object=nearest_object, origin=shifted_point, direction_from_intersection_to_light=direction_from_intersection_to_light, object_hit=object_hit)
+
+        # Reflection
+        color_value += illumination * reflection
+
+        # TODO - testing on whether this is necessary or not.
+        # if not hit_light:
+        #     break
+
+        # Handle reflection and continue
+        reflection *= nearest_object.reflection
+        if reflection == 0:
+            return color_value
+
+        if current_depth == max_depth:
+            return color_value
+
+        # Reset origination and direction to this point and continue recursively
+        origin = shifted_point
+        direction = ray.direction.reflected(other_vector=object_hit.normal_to_surface)
+        return color_value + self.trace_ray(Ray(origin=origin, direction=direction), max_depth=max_depth, current_depth=current_depth + 1, reflection=reflection)
+
+    def calculate_lighting(self,this_object: Primitive, origin: Q_Vector3d, direction_from_intersection_to_light: Q_Vector3d, object_hit: Hit) -> Q_Vector3d:
         CONE_THETA = math.pi / 56.0
         illumination = Q_Vector3d(0, 0, 0)
-        for u in range(lighting_samples):
-            for v in range(lighting_samples):
+        for u in range(self.lighting_samples):
+            for v in range(self.lighting_samples):
                 # Fire a ray towards where the light source is
-                wobbled_direction = OrthoNormalBasis.cone_sample(direction=direction_from_intersection_to_light, cone_theta=CONE_THETA, u=u / lighting_samples, v=v / lighting_samples)
+                wobbled_direction = OrthoNormalBasis.cone_sample(direction=direction_from_intersection_to_light, cone_theta=CONE_THETA, u=u / self.lighting_samples, v=v / self.lighting_samples)
                 ray = Ray(origin=origin, direction=wobbled_direction)
                 illumination += self.calculate_illumination(ray=ray, this_object=this_object, hit=object_hit)
-        return illumination * (1 / (lighting_samples ** 2))
+        return illumination * (1 / (self.lighting_samples ** 2))
 
     def calculate_illumination(self, ray: Ray, this_object: Primitive, hit: Hit) -> Q_Vector3d:
         illumination = Q_Vector3d(0, 0, 0)
